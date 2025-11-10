5f6eb251b8459ac36720938c0157522f2af914da
ricardoSo-sd <rsandovalort@gmail.com>
2025-11-10 10:43:49 -0500

    Carga de archivos index, script, style

---
+++ script.js
@@ -0,0 +1,42 @@
+
+function mayorMenor(n1, n2, n3){
+    if (n1 === n2 && n1 === n3){
+        return "Todos los números sin iguales";
+    }else if (n1 >= n2 && n1 >= n3 ){
+        if(n2 > n3){
+            return `Mayor a menor: ${n1} ${n2} ${n3}
+Menor a mayor ${n3}${n2}${n1}`
+        }else{
+            return `Mayor a mayor ${n1}${n3}${n2}
+Menor a mayor Menor ${n2}${n3}${n1}`
+        }
+
+    }else if (n2 >= n1 && n2 >= n3 ){
+        if(n1 > n3){
+            return `Mayor a menor: ${n2} ${n1} ${n3}
+Menor a mayor ${n3}${n1}${n2}`
+        }else{
+            return `Mayor a mayor ${n2}${n3}${n1}
+Menor a mayor Menor ${n1}${n3}${n2}`
+        }
+    }else if (n3 >= n1 && n3 >= n2 ){
+        if(n1 > n2){
+            return `Mayor a menor: ${n3} ${n1} ${n2}
+Menor a mayor ${n2}${n1}${n3}`
+        }else{
+            return `Mayor a mayor ${n3}${n2}${n1}
+Menor a mayor Menor ${n1}${n2}${n3}`
+        }
+    }
+} 
+
+
+function mostrarAlerta(){
+    const n1 = Number(document.getElementById("numero1").value);
+    const n2 = Number(document.getElementById("numero2").value);
+    const n3 = Number(document.getElementById("numero3").value);
+    let resultado = mayorMenor(n1, n2, n3);
+    alert(resultado)
+
+}
+
